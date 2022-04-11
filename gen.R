9aac5edcb3b7a491c6ba7f4ae254e671dd310249
MW <marcin119a@gmail.com>
2022-04-11 15:01:11 +0200

    thetas dim

--- gen.R
+++ gen.R
@@ -3,7 +3,7 @@ library("heavy")
 
 spots = 841
 types = 10 
-M.sim <- matrix(rtgamma(spots*types, shape = 10, truncation = 20), nrow = types, ncol = spots)
+M.sim <- matrix(rtgamma(spots*types, shape = 10, truncation = 20), nrow = spots, ncol = types)
 write.matrix(M.sim, file="data/thetas_sim_1.csv", sep = ",") 
-M.prior <- matrix(rtgamma(spots*types, shape = 10, truncation = 20), nrow = types, ncol = spots)
+M.prior <- matrix(rtgamma(spots*types, shape = 10, truncation = 20), nrow = spots, ncol = types)
 write.matrix(M.prior, file="data/thetas_prior_1.csv", sep = ",") 
